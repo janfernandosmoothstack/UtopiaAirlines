3e399faa04f1329714c0db69233f2c271447a0f8
jobysanthosh <joby.santhosh@smoothstack.com>
2019-11-22 16:00:57 -0500

    payment almost done

--- src/component/payment.js
+++ src/component/payment.js
@@ -12,54 +12,56 @@ export const Payment = () => {
       <form>
         
       <div class="form-group row">
-        <label for="inputEmail" class="col-sm-2 col-form-label">Name On Card</label>
+        <label for="cardName" class="col-sm-2 col-form-label">Name On Card</label>
       <div class="col-sm-5">
-        <input type="email" class="form-control" id="inputEmail" placeholder="Name"/>
+        <input type="text" class="form-control" id="cardName" placeholder="Name"/>
       </div>
       </div>
 
 
       <div class="form-group row">
-        <label for="inputEmail10" class="col-sm-2 col-form-label">Card Type</label>
+        <label for="cardType" class="col-sm-3 col-form-label">Card Type</label>
       <div class="col-sm-1">
       <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input"/>
-      <label class="custom-control-label" for="customRadioInline1">Visa</label>
+      <label class="custom-control-label" for="customRadioInline1">
+      <img src="https://img.icons8.com/clouds/100/000000/visa.png"/>
+      </label>
       </div>
       <div class="custom-control custom-radio custom-control-inline">
         <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input"/>
-        <label class="custom-control-label" for="customRadioInline2">Master</label>
+        <label class="custom-control-label" for="customRadioInline2"></label>
+        <img src="https://img.icons8.com/cute-clipart/64/000000/mastercard.png"/>
       </div>
       </div>
 
       <div class="form-group row">
-        <label for="inputEmail1" class="col-sm-2 col-form-label">Card Number</label>
+        <label for="cardNumber" class="col-sm-2 col-form-label">Card Number</label>
       <div class="col-sm-5">
-        <input type="email" class="form-control" id="inputEmail1" placeholder="Card Number"/>
+        <input type="text" class="form-control" id="cardNumber" placeholder="Card Number"/>
       </div>
       </div>
       <br></br>
 
       <div class="form-group row">
-        <label for="start" class="col-sm-2 col-form-label">Start month:</label>
-        <label for="inputEmail1" class="col-sm-2 col-form-label">Expiration Date</label>
+        <label for="start" class="col-sm-2 col-form-label">Expiration Date</label>
       <div class="col-sm-5">
       <input type="month" id="start" name="start"
-       min="2018-03" value="2018-05"/>
+       min="2018-03" />
       </div>
       </div>
       <br></br>
 
       <div class="form-group row">
-        <label for="inputEmail2" class="col-sm-2 col-form-label">Security Code</label>
+        <label for="securityCode" class="col-sm-2 col-form-label">Security Code</label>
       <div class="col-sm-1">
-        <input type="email" class="form-control" id="inputEmail2" placeholder="CVC"/>
+        <input type="text" class="form-control" id="securityCode" placeholder="CVC"/>
       </div>
       </div>
       <br></br>
 
       <div class="form-group row">
     <div class="col-sm-10">
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-danger">Submit</button>
     </div>
   </div>
   
