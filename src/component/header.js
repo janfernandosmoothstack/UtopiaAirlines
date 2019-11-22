b66115af3ebe3884cded4df2f2eb19a218e45db1
janfernandosmoothstack <janet.fernando@smoothstack.com>
2019-11-22 11:26:49 -0500

    header and home

--- src/component/header.js
+++ src/component/header.js
@@ -6,12 +6,17 @@ export const Header = () => {
     return (
         <Navbar bg="primary" variant="dark">
             <Navbar.Brand href="/">
-                <img width="80px" height="60px" src="images/ualogo.png" />
+                <img width="80px" height="60px" src="https://imgur.com/P43ESmH.png" alt="logo" />
             </Navbar.Brand>
 
             <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/flights">Flights</Nav.Link>
+            </Nav>
+
+            <Nav className="justify-content-end">
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
+                <Nav.Link href="/signin">Sign In</Nav.Link>
             </Nav>
         </Navbar>
     );
