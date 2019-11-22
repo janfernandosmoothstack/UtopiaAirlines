1b2485e7cb4b122d16e3b094384a75fb88180063
janfernandosmoothstack <janet.fernando@smoothstack.com>
2019-11-22 13:57:22 -0500

    sign in modal

--- src/component/header.js
+++ src/component/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import {SignIn} from './signin.js'
 
 export const Header = () => {
     return (
@@ -12,11 +13,12 @@ export const Header = () => {
             <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/flights">Flights</Nav.Link>
+                <Nav.Link href="/cancel">Cancellation</Nav.Link>
             </Nav>
 
             <Nav className="justify-content-end">
                 <Nav.Link href="/signup">Sign Up</Nav.Link>
-                <Nav.Link href="/signin">Sign In</Nav.Link>
+                <SignIn/>
             </Nav>
         </Navbar>
     );
