849929c8bbb0586ff7a1758e396cacb93babc934
sayana <sayana.shrestha@veritis.com>
2019-12-16 13:25:21 -0500

    testing

--- src/actions/accountActions.js
+++ src/actions/accountActions.js
@@ -2,29 +2,6 @@ import React from 'react';
 import axios from "axios";
 class AccountAction extends React.Component {
    
-    static getUser(hash, props) {
-        
-        return (
-            axios({
-                method: 'get',
-                url: 'http://utopiaairlinelb-1335414957.us-east-2.elb.amazonaws.com/users/verified',
-                headers: {'Content-Type': 'application/json',
-                  'username': event.target.username.value, 'password': hash}
-                })
-                .then(function (response) {
-                    //handle success
-                    console.log(response);
-                       alert("Login Success");
-                       props.history.push('/flights');
-                })
-                .catch(function (response) {
-                    //handle error
-                  console.log(response);
-                alert("Incorrect login credentials!");
-                })
-        )
-    };
-  
     static createUser(user, hash, props) {
         return (
             axios({
@@ -42,6 +19,29 @@ class AccountAction extends React.Component {
                 .catch(function (response) {
                   console.log(response);
                 alert("Please use different Username");
+                })
+        )
+    }
+
+    static getUser(hash, props) {
+        
+        return (
+            axios({
+                method: 'get',
+                url: 'http://localhost:8090/users/verified',
+                headers: {'Content-Type': 'application/json',
+                  'username': event.target.username.value, 'password': hash}
+                })
+                .then(function (response) {
+                    //handle success
+                    console.log(response);
+                       alert("Login Success");
+                       props.history.push('/flights');
+                })
+                .catch(function (response) {
+                    //handle error
+                  console.log(response);
+                alert("Incorrect login credentials!");
                 })
         )
     };
